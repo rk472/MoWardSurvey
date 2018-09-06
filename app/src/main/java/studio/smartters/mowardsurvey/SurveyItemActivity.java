5ef08a1ef850f2074b1c4996f7b0ffcd0f5fcd06
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-09-06 14:18:22 +0530

    InitialCommit

--- app/src/main/java/studio/smartters/mowardsurvey/SurveyItemActivity.java
+++ app/src/main/java/studio/smartters/mowardsurvey/SurveyItemActivity.java
@@ -1,7 +1,12 @@
 package studio.smartters.mowardsurvey;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SurveyItemActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class SurveyItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_item);
+        String jsonData = getIntent().getExtras().getString("json_data");
+        Log.i("MyData",jsonData);
+        Toast.makeText(this, jsonData, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.save_survey, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_save) {
+            Toast.makeText(this, "Save Clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
