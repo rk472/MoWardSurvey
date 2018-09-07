6dbf4b6834d7b978d38034e5366997beaba70929
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-09-07 21:00:26 +0530

    InitialCommit

--- app/src/main/java/studio/smartters/mowardsurvey/ViewDataActivity.java
+++ app/src/main/java/studio/smartters/mowardsurvey/ViewDataActivity.java
@@ -17,10 +17,10 @@ public class ViewDataActivity extends AppCompatActivity {
         etSearch.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+
             }
             @Override
             public void afterTextChanged(Editable s) {
