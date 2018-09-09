ddc646a15cc0da6460af0cdfbdf8512b5b597192
rk472 <33664106+rk472@users.noreply.github.com>
2018-09-09 17:28:15 +0530

    survey changed

--- app/src/main/java/studio/smartters/mowardsurvey/HelpDetailsActivity.java
+++ app/src/main/java/studio/smartters/mowardsurvey/HelpDetailsActivity.java
@@ -22,7 +22,6 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
